b5fb6b7229b14f09249a9d761dfd1b1e4390b39c
Peter Alexander <makeroftools@creativedevelopment.systems>
2026-01-17 11:23:40 -0700

    toy graphql example working

---
+++ src/bin/graphql/main.rs
@@ -0,0 +1,40 @@
+use std::error::Error;
+
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema };
+use async_graphql_axum::GraphQL;
+use axum::{
+    Router,
+    response::{self, IntoResponse},
+    routing::get
+};
+
+use tokio::net::TcpListener;
+
+
+struct Query;
+
+#[Object]
+impl Query {
+    async fn howdy(&self) -> &'static str {
+        "partner"
+    }
+}
+
+async fn graphiql() -> impl IntoResponse {
+    // Html(GraphiQLSource::build().finish())
+    response::Html(GraphiQLSource::build().endpoint("/").finish())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // create the schema
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+
+    // start the http server
+    let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
+    println!("GraphiQL: http://localhost:8000");
+    axum::serve(TcpListener::bind("127.0.0.1:8000").await.unwrap(), app)
+        .await
+        .unwrap();
+    Ok(())
+}
